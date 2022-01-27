8bb2c387d13bb1a76b4629560912e05d139e36d1
Alexis Barros <apenasalexis@gmail.com>
2022-01-26 21:14:43 -0300

    bugfix: index.controller - remove deleted ref

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,6 +2,7 @@ export * from './acl-action.controller';
 export * from './acl-has-actions.controller';
 export * from './acl.controller';
 export * from './auth.controller';
+export * from './invitation-has-permissions.controller';
 export * from './invitation.controller';
 export * from './module.controller';
 export * from './permission-has-acls.controller';
@@ -10,5 +11,4 @@ export * from './ping.controller';
 export * from './project.controller';
 export * from './user-has-permissions.controller';
 export * from './user.controller';
-export * from './invitation-has-permissions.controller';
-export * from './project-permission.controller';
+
