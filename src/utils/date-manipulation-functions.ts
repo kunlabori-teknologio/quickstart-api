fe98937d27fd3cc979b5d9abe787fbe3090ca3f6
Alexis Barros <apenasalexis@gmail.com>
2023-01-13 15:17:50 -0300

    feat: create convertStringFieldsToDate

--- src/utils/date-manipulation-functions.ts
+++ src/utils/date-manipulation-functions.ts
@@ -8,3 +8,18 @@ export function convertBirthdayStringToDate(birthday: string): Date {
 export function theDatesMatch(date1: Date, date2: string): boolean {
   return date1.toISOString().substring(0, 10) === date2.substring(0, 10);
 }
+
+export function convertStringFieldsToDate(object: any, dateFieldsArray: string[]): any{
+
+  const fields = Object.keys(object);
+
+  for (let fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
+    const field = fields[fieldIndex];
+
+    if(Array.isArray(object[field])){
+      object[field] = object[field].map((obj:any) => convertStringFieldsToDate(obj, dateFieldsArray))
+    } else if(dateFieldsArray.includes(field) && object[field]) object[field] = new Date(object[field])
+  }
+
+  return object;
+}
